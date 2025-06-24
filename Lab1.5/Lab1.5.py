4aa7e7fa436a1d115b53f92f768da3445f0a94cb
ib.erdakov <i.erdakov.b@gmail.com>
2025-06-24 16:31:03 +0300

    version 1

---
+++ Lab1.5/Lab1.5.py
@@ -0,0 +1,18 @@
+import glob
+list_ipaddress=[]
+listfiles= glob.glob('*.log')
+
+for file in listfiles:
+    with open(file) as f:
+         for row in f:
+             if "ip address" in row:
+                 if not "guest" in row:
+                     if  not "sub" in row:
+                         if not "no" in row:
+                            list_ipaddress.append(row)
+#list_ipaddress.remove("no")
+uniq = list(set(list_ipaddress))
+sorted(uniq)
+
+for ip_add in uniq:
+    print(ip_add)
