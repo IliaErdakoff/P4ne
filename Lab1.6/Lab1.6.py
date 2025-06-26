25d94d6df45349bd77ff920a39f26ce693d51279
ib.erdakov <i.erdakov.b@gmail.com>
2025-06-26 18:45:14 +0300

    version 2

--- Lab1.6/Lab1.6.py
+++ Lab1.6/Lab1.6.py
@@ -1,8 +1,6 @@
 
 import glob, ipaddress, re
 
-
-
 list_ipaddress=[]
 ipaddress_new=[]
 listfiles= glob.glob('*.log')
@@ -10,12 +8,11 @@ listfiles= glob.glob('*.log')
 for file in listfiles:
     with open(file) as f:
         for row in f:
-            if "ip address" in row:
-                list_ipaddress.append(row)
-for row in list_ipaddress:
-    m = re.search(r"([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})", row )
-    if m:
-        ip_mask = m.group(1)+"/" +m.group(2)
-        ipaddress_new.append(ipaddress.IPv4Interface(ip_mask))
-print(ipaddress_new)
+            m = re.search(r"ip address ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}) ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})", row )
+            if m:
+                ipaddress_new.append(ipaddress.IPv4Interface(m.group(1,2)))
+
+for ip in ipaddress_new:
+    print(ip)
+
 
